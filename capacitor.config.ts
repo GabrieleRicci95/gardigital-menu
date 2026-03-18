a9c5b804d7d48557ba645e7e6f1be85ac7f694da
Gabriele Ricci <gabrielericci234@gmail.com>
2026-03-18 11:37:03 +0100

    Build: Rinomina App in 'SoloMenu' (#34)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.gardigital.menu',
-  appName: 'Gardigital Menu',
+  appName: 'SoloMenu',
   webDir: 'public',
   server: {
     url: 'https://www.gardigital.it/login',
